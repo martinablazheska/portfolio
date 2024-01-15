5574c6a451c5b1ff26b4a5c8a9dfbe77090b1f3a
Martina Blazheska <123024212+martinablazheska@users.noreply.github.com>
2024-01-15 19:59:59 +0100

    Updated about me

--- src/data/about.ts
+++ src/data/about.ts
@@ -9,22 +9,13 @@ const aboutMe = [
     paragraph:
       "My passion for problem-solving and optimization led me to embark on the path of frontend development. To do this, I complete comprehensive courses and spent the past year immersed in projects to deepen my skills.",
   },
+
   {
     id: 3,
     paragraph:
-      "Recently, I started working on my first real frontend project, which involves intricate tasks such as API integration, user authentication and establishing seamless communication between users.",
-  },
-  {
-    id: 4,
-    paragraph:
-      "Beyond coding, I find joy in strength training, sports, and the outdoors. These activities play a great role in my grounded and clear-minded approach to life and work.",
-  },
-  {
-    id: 5,
-    paragraph:
       "With a team-oriented mindset and a quick grasp of new concepts, I'm committed to continuous growth in this ever-evolving field.",
   },
-  { id: 6, paragraph: "Let's build something incredible together!" },
+  { id: 4, paragraph: "Let's build something incredible together!" },
 ];
 
 export default aboutMe;
