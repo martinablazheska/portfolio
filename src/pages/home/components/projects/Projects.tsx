18130e0af37a8f37cf15fc866401c40e2453e67b
Martina Blazheska <damjan.stojanovski123@gmail.com>
2024-12-20 17:45:11 +0100

    Added footer component

---
+++ src/pages/home/components/projects/Projects.tsx
@@ -0,0 +1,12 @@
+import SectionWrapper from "@/components/ui/SectionWrapper";
+import SectionHeading from "@/components/ui/SectionHeading";
+
+const Projects = () => {
+  return (
+    <SectionWrapper id="Projects" className="flex flex-col items-center">
+      <SectionHeading heading="Projects" />
+    </SectionWrapper>
+  );
+};
+
+export default Projects;
